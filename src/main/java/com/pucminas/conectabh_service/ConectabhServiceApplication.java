a6d19c9c17496986d2eddbad96fa88e262e28741
lucborges <lucaspy07@gmail.com>
2025-03-16 15:56:58 -0300

    Inicialização do projeto

---
+++ src/main/java/com/pucminas/conectabh_service/ConectabhServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pucminas.conectabh_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectabhServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectabhServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pucminas/conectabh_service/ConectabhServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucminas.conectabh_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectabhServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
