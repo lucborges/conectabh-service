ca7988228c3f2d007c91b7838fe3a4f6125a7ee6
lucborges <lucaspy07@gmail.com>
2025-05-09 22:34:27 -0300

    feat: cors config

--- src/main/java/com/pucminas/conectabh_service/adapter/entityToData/ReservationToReservationData.java
+++ src/main/java/com/pucminas/conectabh_service/adapter/entityToData/ReservationToReservationData.java
@@ -2,12 +2,7 @@ package com.pucminas.conectabh_service.adapter.entityToData;
 
 import com.pucminas.conectabh_service.adapter.Adapter;
 import com.pucminas.conectabh_service.domain.Reservation;
-import com.pucminas.conectabh_service.repository.UserRepository;
-import com.pucminas.conectabh_service.repository.WorkspaceRepository;
 import com.pucminas.conectabh_service.repository.data.ReservationData;
-import com.pucminas.conectabh_service.repository.data.UserData;
-import com.pucminas.conectabh_service.repository.data.WorkspaceData;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
